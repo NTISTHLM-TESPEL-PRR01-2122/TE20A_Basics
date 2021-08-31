9d223bd001dafa75c2bac8a7153c888c5e098c79
Mikael Bergström <krank23@gmail.com>
2021-08-31 15:00:21 +0200

    Initial commit

---
+++ TE20A_Basics/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+// Console.WriteLine("Varför måste man vara musikalisk för att äta banan?");
+// Console.WriteLine("Man måste kunna SKALAN!!! =) lololol");
+
+string name;
+
+Console.WriteLine("Vad heter du?");
+
+name = "Micke";
+name = Console.ReadLine();
+
+Console.WriteLine($"Hej, {name}! Vad ful du är idag!");
+
+
+Console.ReadLine();
